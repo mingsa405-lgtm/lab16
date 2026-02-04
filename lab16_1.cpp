75a6b91ef990f2813167431b5b8fd921c57bfc5c
ming <mingsa405@gmail.com>
2026-02-04 14:28:22 +0700

    up

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,42 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *a,int M, int N){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%N==0) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double *a,int M,int N){
+    for(int i = 0; i < N*M; i++){
+          a[i]= (rand()%101)/100.0;
+    }
+}
+void findColSum(const double *a, double *b, int m, int n){
+    double sum;
+
+    for(int i = 0; i < n; i++){     
+        sum = 0;
+        for(int j = 0; j < m; j++){  
+            sum += a[j*n + i];
+        }
+        b[i] = sum;                  
+    }
+}
+void findRowSum(const double *a,double *b,int m,int n){
+    double sum=0;
+    int r=0;
+    for(int i=0;i<m*n;i++){
+        sum=sum+a[i];
+        if((i+1)%n==0){b[r]=sum;
+            sum=0;
+            r+=1;
+        }
+       
+        
+        
+    }
+    }
+
